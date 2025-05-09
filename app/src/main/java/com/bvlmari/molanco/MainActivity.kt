0302ca41a5de4db8a9a7a497d7ce4363e0c07835
Mariano Perez Piña <60488652+bvlmari@users.noreply.github.com>
2025-05-08 22:06:16 -0700

    Add top bar buttons

--- app/src/main/java/com/bvlmari/molanco/MainActivity.kt
+++ app/src/main/java/com/bvlmari/molanco/MainActivity.kt
@@ -1,16 +1,42 @@
 package com.bvlmari.molanco
 
+import android.content.Intent
+import android.media.Image
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val buttonBack = findViewById<ImageButton>(R.id.buttonBack)
+        val buttonFavorites = findViewById<ImageButton>(R.id.buttonFavorites)
+        val buttonOptions = findViewById<ImageButton>(R.id.buttonOptions)
+        buttonBack.setOnClickListener{
+            finish()
+        }
+        buttonFavorites.setOnClickListener{
+            val intent = Intent(this, FavoritesActivity::class.java)
+            startActivity(intent)
+        }
+        buttonOptions.setOnClickListener{
+            val shareIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "Yooo")
+                type = "text/plain"
+            }
+            startActivity(Intent.createChooser(shareIntent, "Share via"))
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
